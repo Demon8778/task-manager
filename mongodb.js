ea9d144832418b76d2c7f12c21b12ee8d741c1ec
Kirankumar Ahir <kirankatariya8778@gmail.com>
2019-04-28 22:50:17 +0530

    Updating documents

--- mongodb.js
+++ mongodb.js
@@ -1,5 +1,8 @@
-const mongodb = require('mongodb');
-const MongoClient = mongodb.MongoClient;
+// const mongodb = require('mongodb');
+// const MongoClient = mongodb.MongoClient;
+// const ObjectID = mongodb.ObjectID;
+
+const { MongoClient, ObjectID } = require('mongodb');
 
 const connectionURL = 'mongodb://127.0.0.1:27017';
 const databaseName = 'task-manager';
@@ -19,19 +22,67 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
         
     //     console.log(result.ops);
     // });
-    db.collection('users').insertMany([
-        {
-            name: 'Kalpesh',
-            age: 27
-        },
-        {
-            name: 'Kalpesh',
-            age: 27
+    // db.collection('users').insertMany([
+    //     {
+    //         name: 'Kalpesh',
+    //         age: 27
+    //     },
+    //     {
+    //         name: 'Kalpesh',
+    //         age: 27
+    //     }
+    // ], (error, result) => {
+    //     if (error) 
+    //         return console.log('Unable to insert documents!');
+
+    //     console.log(result.ops);
+    // });
+
+    // db.collection('tasks').insertMany([
+    //     {
+    //         description: 'Go for a walk!',
+    //         completed: false
+    //     }, {
+    //         description: 'Do anything you want to do!',
+    //         completed: false
+    //     }
+    // ], (error, result) => {
+    //     if (error) 
+    //         return console.log('Unable to insert records!');
+    //     console.log(result.ops)
+    // });
+
+    // db.collection('users').find({ age: 27 }).toArray((error, users) => {
+    //     if (error) 
+    //         return console.log('Unable to find users!');
+    //     console.log(users);
+    // });
+
+    // db.collection('users').find({ age: 27 }).count((error, count) => {
+    //     if (error)
+    //         return console.log('Unable to find users!');
+    //     console.log(count);
+    // });
+
+    // db.collection('tasks').find({ completed: false }).toArray((error, tasks) => {
+    //     if (error)
+    //         return console.log('Unable to find users!');
+    //     console.log(tasks);
+    // });
+
+    // db.collection('users').findOne({ _id: new ObjectID('5cc55ce70a359631007c47d7') }, (error, user) => {
+    //     console.log(user);
+    // });
+
+    db.collection('tasks').updateMany({
+        completed: false
+    }, {
+        $set: {
+            completed: true
         }
-    ], (error, result) => {
-        if (error) 
-            return console.log('Unable to insert documents!');
-            
-        console.log(result.ops);
+    }).then((result) => {
+        console.log(result);
+    }).catch((error) => {
+        console.log(error);
     })
 });

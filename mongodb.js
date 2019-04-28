07a54f2261da1c352f2a3e012c805183953cc9d7
Kirankumar Ahir <kirankatariya8778@gmail.com>
2019-04-28 13:28:06 +0530

    Adding multiple documents

--- mongodb.js
+++ mongodb.js
@@ -8,6 +8,30 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
     if (error) 
         return console.log('Unable to connect to databse!');
 
-    
+    const db = client.db(databaseName);
 
+    // db.collection('users').insertOne({
+    //     name: 'Kiran',
+    //     age: 22
+    // }, (error, result) => {
+    //     if (error) 
+    //         return console.log('Unable to insert user!');
+        
+    //     console.log(result.ops);
+    // });
+    db.collection('users').insertMany([
+        {
+            name: 'Kalpesh',
+            age: 27
+        },
+        {
+            name: 'Kalpesh',
+            age: 27
+        }
+    ], (error, result) => {
+        if (error) 
+            return console.log('Unable to insert documents!');
+            
+        console.log(result.ops);
+    })
 });
